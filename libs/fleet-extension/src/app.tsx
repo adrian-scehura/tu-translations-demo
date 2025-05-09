f325753f96d6d8384f6faab31b473ef5b5dbf0c7
Adrian Scehura <adrian.scehura@gmail.com>
2025-05-09 14:23:02 +0200

    add table

--- libs/fleet-extension/src/app.tsx
+++ libs/fleet-extension/src/app.tsx
@@ -1,33 +1,66 @@
-import React from 'react';
-import {
-  Card,
-  CardBody,
-  CardHeader,
-  Text,
-  Button,
-  Alert,
-} from '@trackunit/react-components';
+import { createColumnHelper, Table, useTable } from '@trackunit/react-table';
+import { TextCell } from '@trackunit/react-table-base-components';
+
+interface DataType {
+  id: string;
+  name: string;
+  assetType: string;
+  brand: string;
+}
+
+const defaultData: DataType[] = [
+  {
+    id: '1',
+    name: 'SM Smoketest Asset 1',
+    assetType: 'machine',
+    brand: 'Manitou',
+  },
+  { id: '2', name: '3219', assetType: 'machine', brand: 'Skyjack' },
+  { id: '3', name: '3226', assetType: 'machine', brand: 'Skyjack' },
+];
+
+const columnHelper = createColumnHelper<Partial<DataType>>();
+
+const columns = [
+  columnHelper.accessor((row) => row?.name, {
+    cell: ({ row: { original } }: { row: { original: Partial<DataType> } }) => (
+      <TextCell content={original?.name} />
+    ),
+    header: 'Name',
+    id: 'name',
+    size: 250,
+  }),
+  columnHelper.accessor((row) => row?.assetType, {
+    cell: ({ row: { original } }: { row: { original: Partial<DataType> } }) => (
+      <TextCell content={original?.assetType} />
+    ),
+    header: 'Asset Type',
+    id: 'assetType',
+  }),
+  columnHelper.accessor((row) => row?.brand, {
+    cell: ({ row: { original } }: { row: { original: Partial<DataType> } }) => (
+      <TextCell content={original?.brand} />
+    ),
+    header: 'Brand',
+    id: 'brand',
+  }),
+];
+
+const pagination = {
+  nextPage: () => {}, // Handle next page
+  previousPage: () => {}, // Handle previous page
+  isLoading: false,
+  pageInfo: {
+    count: defaultData.length,
+  },
+};
 
 export const App = () => {
-  return (
-    <div className="w-full h-full grid place-content-center" data-testid="app">
-      <Card className="w-full">
-        <CardHeader
-          heading="Welcome to your app extension"
-          subHeading="Change the code in this file as you see fit"
-        />
-        <CardBody>
-          <Alert color="success">
-            You are now ready to develop your app extension.
-          </Alert>
-          <Text> We also have buttons</Text>
-          <Button>Click me!</Button>
-          <Text>
-            More info and component found in storybook: <br />
-            https://apps.iris.trackunit.com/storybook/
-          </Text>
-        </CardBody>
-      </Card>
-    </div>
-  );
+  const { table } = useTable<Partial<DataType>>({
+    data: defaultData,
+    columns,
+    enableSorting: false,
+  });
+
+  return <Table<Partial<DataType>> {...table} pagination={pagination} />;
 };
